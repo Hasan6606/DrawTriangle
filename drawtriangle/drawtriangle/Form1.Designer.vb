b4410e891ed8c80e930b9cfbbe99141ad8e1b159
Hasan6606 <62176480+Hasan6606@users.noreply.github.com>
2020-06-17 11:24:32 +0300

    Visual Basic programında girilen açı ve kenar değerlerini hesaplayarak dik üçgen çizme

---
+++ drawtriangle/drawtriangle/Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(607, 330)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Üçgeni Çiz"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(582, 245)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(492, 248)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Açıyı giriniz:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(350, 298)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(206, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Açının gördüğü kenar uzulunluğunu giriniz:"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(582, 291)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(984, 351)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox2 As TextBox
+End Class
---
+++ drawtriangle/drawtriangle/Form1.vb
@@ -0,0 +1,43 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim sindeger As Double = Val(TextBox1.Text) 'Bu satırda  kullanıcan hesaplanıcak sinüs değerinin açısal olarak bir sayısal veri alınır.
+        Dim sinacideger As Double = Math.Sin(sindeger) 'Bu satırda ise alınan açısal veriyi "Math()" kütüphanesinin "sin()" metodu ile açının sinüs değeri hesaplanır.
+        Dim akenar As Double = Val(TextBox2.Text) 'Kullanıcıdan açının görmüş olduğu kenarın uzunluğunu alır.
+        Dim bkenar As Double = (akenar / (sindeger)) 'Sinüs=Karşı/Hipotenüs olduğu için burda hipotenüs yalnız bırakarak değerine erişmiş olduk.
+
+
+
+        Dim mypen As Pen  'Burda ise üçgeni çizicek kalemimizi tanımladık.
+        Dim A As New Point(150, 150) 'Bu satırda ise üçgenin bir noktasını belirledik koordinat mantığıyla.
+        Dim B As New Point(150, bkenar) 'Bu satırda ise üçgenin bir noktasını belirledik koordinat mantığıyla.
+        Dim C As New Point(bkenar, 150) 'Bu satırda ise üçgenin bir noktasını belirledik koordinat mantığıyla.
+        Dim mypoint As Point() = {A, B, C} 'Burda ise noktalarımızı kümeleme işlemi yapmış oldu.
+        mypen = New Pen(Drawing.Color.Red, 10) 'Burda ise kalemimizin rengini kalınlığı ayarlanmıştır.
+        Dim mygrap As Graphics = Me.CreateGraphics 'Burda ise grafik çercevesinde koordinat sistemi mantığıyla değişken tanımladık.
+        mygrap.DrawPolygon(mypen, mypoint)  'Bu satırda ise grafik "DrawPolygon()" metodu aracılığıyla kalemimizi ve noktalarımızı tanımlayıp üçgeni çizdirmiş olduk.
+
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+
+
+
+
+
+
+
+
+
+    End Sub
+End Class
